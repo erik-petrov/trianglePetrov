026226f859d9cb666a8d1562c958e1abbdb225be
opilane <opilane@HP-CZC6297B8L>
2021-11-10 14:00:06 +0200

    Add project files.

---
+++ Triangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trianglePetrov
+{
+    class Triangle
+    {
+        private double a,b,c;
+        public Triangle(double A, double B, double C)
+        {
+            a = A;
+            b = B;
+            c = C;
+        }
+        public double Perimeter()
+        {
+            return a+b+c;
+        }
+        public double Surface()
+        {
+            double p = Perimeter() / 2;
+            return Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+        }
+        public bool RealityCheck()
+        {
+            return (a > b + c) && (b > c + a) && (c > b + a) ? false : true;
+        }
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+    }
+}
